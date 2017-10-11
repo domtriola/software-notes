cb5c222c64d4635b3f825e9c9f5417dd41b3e38e
Dominick Triola <dominick@ignovadesign.com>
2017-10-10 22:19:41 -0700

    Complete array exercises

--- ruby/basic-exercises/lib/exercises.rb
+++ ruby/basic-exercises/lib/exercises.rb
@@ -1,15 +1,78 @@
+require 'set'
+
 # Each
+class Array
+  def my_each(&prc)
+    length.times { |index| prc.call(self[index]) }
+    self
+  end
+end
 
 # Reduce
+class Array
+  def my_reduce(seed = nil, &prc)
+    result = seed || self[0]
+
+    if seed
+      index = 0
+    else
+      index = 1
+    end
+
+    while index < length
+      result = prc.call(result, self[index])
+
+      index += 1
+    end
+
+    result
+  end
+end
 
 # Unique
+class Array
+  def my_uniq
+    seen = Set.new
+    result = []
+
+    each do |el|
+      result << el unless seen.include?(el)
+      seen.add(el)
+    end
+
+    result
+  end
+end
 
 # Flatten
+class Array
+  def my_flatten
+    result = []
+
+    each do |el|
+      if el.class == Array
+        result.concat(el.my_flatten)
+      else
+        result << el
+      end
+    end
+
+    result
+  end
+end
 
 # Fizz-Buzz
+def fizz_buzz(max)
+end
 
 # Fibonacci
+def fibonacci(max)
+end
 
 # Merge Sort
+def merge_sort(array)
+end
 
 # Quick Sort
+def quick_sort(array)
+end
--- ruby/basic-exercises/spec/exercises_spec.rb
+++ ruby/basic-exercises/spec/exercises_spec.rb
@@ -3,10 +3,54 @@ require 'exercises'
 
 describe Array do
   describe '#my_each' do
-    it "should call the given proc with each item of the array"
+    it "should call the given block with each item of the array" do
+      expect do |block|
+        [1, 2, 3].my_each(&block)
+      end.to yield_successive_args(1, 2, 3)
+    end
   end
 
   describe '#my_reduce' do
-    it "should reduce the items of the array with the given proc"
+    it "should reduce the items of the array with the given proc" do
+      result = [1, 2, 3, 4].my_reduce { |accum, el| accum + el }
+
+      expect(result).to eq(10)
+    end
+
+    it "should use the given argument given as a seed if present" do
+      result = [1, 2, 3, 4].my_reduce(11) { |accum, el| accum + el }
+
+      expect(result).to eq(21)
+    end
+  end
+
+  describe '#my_uniq' do
+    it "should return a copy of the array without duplicate elements" do
+      result = [0, 1, 1, 2, 3, 2, 2, 4].my_uniq
+
+      expect(result).to eq([0, 1, 2, 3, 4])
+    end
+
+    it "does not modify the original array" do
+      original = [0, 1, 1, 2, 3, 2, 2, 4]
+      original.my_uniq
+
+      expect(original).to eq([0, 1, 1, 2, 3, 2, 2, 4])
+    end
+  end
+
+  describe '#my_flatten' do
+    it "should return a flattened copy of the array" do
+      result = [1, [2, [3, [4]]], [5, 6]].flatten
+
+      expect(result).to eq([1, 2, 3, 4, 5, 6])
+    end
+
+    it "does not modify the original array" do
+      original = [1, [2, [3, [4]]], [5, 6]]
+      original.my_uniq
+
+      expect(original).to eq([1, [2, [3, [4]]], [5, 6]])
+    end
   end
 end
