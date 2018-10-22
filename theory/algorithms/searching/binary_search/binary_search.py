b7a3ac5554091f52159657589fb685e42f2a7fb9
Dominick Triola <dominick@ignovadesign.com>
2018-10-21 18:00:37 -0700

    Setup binary search scaffold

---
+++ theory/algorithms/searching/binary_search/binary_search.py
@@ -0,0 +1,2 @@
+def binary_search(items, target):
+    pass
---
+++ theory/algorithms/searching/binary_search/binary_search_test.py
@@ -0,0 +1,27 @@
+import unittest
+
+from theory.algorithms.sorting.util import create_consecutive_list
+from theory.algorithms.searching.binary_search.binary_search import binary_search
+
+class TestBinarySearch(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestBinarySearch, self).__init__(*args, **kwargs)
+        self.maxDiff = None
+
+    def test_it_searches_small_arrays(self):
+        items = [1, 2, 3]
+        self.assertEqual(binary_search(items, 1), 0)
+
+        items = [1, 2, 3]
+        self.assertEqual(binary_search(items, 2), 1)
+
+        items = [1, 2, 3]
+        self.assertEqual(binary_search(items, 3), 2)
+
+    def test_it_searches_large_arrays(self):
+        items = create_consecutive_list(1000)
+        self.assertEqual(binary_search(items, 978), 977)
+
+
+if __name__ == '__main__':
+    unittest.main()
