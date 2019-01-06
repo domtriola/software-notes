0f9ec7287153643083f15cce0edaed0f3b2c0ad5
Dominick Triola <dominick@ignovadesign.com>
2019-01-06 14:54:55 -0800

    Refactor dfs

--- theory/algorithms/searching/depth_first_search/depth_first_search.py
+++ theory/algorithms/searching/depth_first_search/depth_first_search.py
@@ -5,16 +5,13 @@ def depth_first_search(root, target):
     Expects a node to have 'value', 'left' and 'right' attributes
     The left and right attributes are either nodes or None
     """
-    if root and root.value == target:
+    if not root:
+        return None
+
+    if root.value == target:
         return root
 
-    queue = []
-    if root.left:
-        queue.append(root.left)
-    if root.right:
-        queue.append(root.right)
-
-    for node in queue:
+    for node in [root.left, root.right]:
         found_node = depth_first_search(node, target)
 
         if found_node and found_node.value == target:
