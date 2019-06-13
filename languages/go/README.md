7b421e07a99d0af53395c265fc9ec9063dbc90df
Dominick Triola <dominicktriola@gmail.com>
2019-06-12 19:52:29 -0700

    Create Go readme draft and exercises

---
+++ languages/go/README.md
@@ -0,0 +1,30 @@
+# Python
+
+* [Overview](#overview)
+* [Getting Started](#getting-started)
+* [Key Features](#key-features)
+
+## Overview
+
+TODO
+
+## Getting Started
+
+[Official Site](https://golang.org)
+
+* [Install Go]() <- TODO
+* Set working go path: TODO
+* Compile:
+  * `go build`: creates executables in same location as source files
+  * or `go install` creates executables in `bin/`
+* Clean up: `go clean -i`
+* Run a single file: `go run path/filename.go`
+
+## Key Features
+
+* **Programming Paradigm**: Imperative
+* **Typing Discipline**: Static
+
+### Concurrency
+
+### Channels
---
+++ languages/go/src/exercises/exercises.go
@@ -0,0 +1,65 @@
+package main
+
+import "fmt"
+import "strconv"
+
+func main() {
+	fmt.Printf("Running basic exercise tests...\n")
+
+	fmt.Printf("-------------------------------\n")
+	fmt.Printf("FizzBuzz:\n")
+	fizzBuzz(16)
+
+	fmt.Printf("-------------------------------\n")
+	fmt.Printf("Fibonacci:\n")
+	fmt.Printf("%d\n", fibonacci(1))
+	fmt.Printf("%d\n", fibonacci(2))
+	fmt.Printf("%d\n", fibonacci(3))
+	fmt.Printf("%d\n", fibonacci(4))
+	fmt.Printf("%d\n", fibonacci(5))
+	fmt.Printf("%d\n", fibonacci(6))
+}
+
+// Each
+
+// Reduce
+
+// Unique
+
+// Flatten
+
+// Fizz-Buzz
+func fizzBuzz(max int) {
+	// TODO: how to declare array from input variable?
+	// const aSize = max
+	// var result [aSize]string
+
+	result := make([]string, max)
+	for i := 1; i <= max; i++ {
+		switch {
+		case i % 15 == 0:
+			result[i - 1] = "FizzBuzz"
+		case i % 3 == 0:
+			result[i - 1] = "Fizz"
+		case i % 5 == 0:
+			result[i - 1] = "Buzz"
+		default:
+			result[i - 1] = strconv.Itoa(i)
+		}
+	}
+
+	for _, val := range result {
+		fmt.Printf("%s\n", val)
+	}
+}
+
+// Fibonacci
+func fibonacci(num int) (fib int) {
+	if num < 2 {
+		return 1
+	}
+
+	return fibonacci(num - 2) + fibonacci(num - 1)
+}
+
+// Merge Sort
