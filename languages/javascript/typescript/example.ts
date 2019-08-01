f91e6066a21bf2df1bfed6fb369f7802c9bf78ac
Dominick Triola <dominicktriola@gmail.com>
2019-08-01 14:30:55 -0700

    Add typescript example

--- languages/javascript/typescript/example.ts
+++ languages/javascript/typescript/example.ts
@@ -1,7 +1,48 @@
-function hello(name) {
-  return `hello ${name}`;
+/*
+ * Vanilla
+ ****************************************/
+function hello(someone) {
+  return `hello ${someone.firstName} ${someone.lastName}`;
 }
 
-const userName = 'Bud';
+const cat = {
+  firstName: 'Mr.',
+  lastName: 'Meow',
+};
 
-console.log(hello(userName));
+console.log(hello(cat));
+// hello Mr. Meow
+
+/*
+ * TypeScript
+ ****************************************/
+interface Someone {
+  firstName: string;
+  lastName: string;
+}
+
+function helloTyped(someone: Someone) {
+  return `hello ${someone.firstName} ${someone.lastName}`;
+}
+
+const catB = {
+  firstName: 'Mr.',
+  lastName: 'Meow',
+};
+
+console.log(helloTyped(catB));
+// hello Mr. Meow
+
+class Cat {
+  fullName: string;
+
+  constructor(public firstName: string, public middleName: string, public lastName: string) {
+    this.fullName = `${firstName} ${middleName} ${lastName}`;
+  }
+}
+
+const catC = new Cat('Mr.', 'Meow', 'Meow');
+
+// Still works, because 'Cat' conforms to 'Someone' interface
+console.log(helloTyped(catC));
+// hello Mr. Meow
