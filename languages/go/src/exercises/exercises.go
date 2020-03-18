5a5a7689c90f1ab7fc26d50e2d37d93aa0ae3eb9
Dominick Triola <dtriola@twilio.com>
2020-03-18 16:18:07 -0700

    Add basic exercises

--- languages/go/src/exercises/exercises.go
+++ languages/go/src/exercises/exercises.go
@@ -7,6 +7,28 @@ func main() {
 	fmt.Printf("Running basic exercise tests...\n")
 
 	fmt.Printf("-------------------------------\n")
+	fmt.Printf("ForEach:\n")
+	forEach([]string{"a", "b", "c"}, func(el string) {
+		fmt.Println(el)
+	})
+
+	fmt.Printf("-------------------------------\n")
+	fmt.Printf("Reduce:\n")
+	sum := reduce([]int{1, 2, 3, 4}, func(accum int, num int) int {
+		return accum + num
+	}, 10)
+	fmt.Printf("sum: %d\n", sum)
+	product := reduce([]int{1, 2, 3, 4}, func(accum int, num int) int {
+		return accum * num
+	}, 1)
+	fmt.Printf("product: %d\n", product)
+
+	fmt.Printf("-------------------------------\n")
+	fmt.Printf("Unique:\n")
+	uniques := unique([]string{"1", "2", "1", "1", "2", "3", "4", "5", "3"})
+	fmt.Printf("uniques: %v\n", uniques)
+
+	fmt.Printf("-------------------------------\n")
 	fmt.Printf("FizzBuzz:\n")
 	fizzBuzz(16)
 
@@ -21,10 +43,38 @@ func main() {
 }
 
 // Each
+func forEach(arr []string, f func(el string)) {
+	for _, el := range arr {
+		f(el)
+	}
+}
 
 // Reduce
+func reduce(arr []int, f func(accum int, num int) int, seed int) int {
+	result := seed
+
+	for i := 0; i < len(arr); i++ {
+		result = f(result, arr[i])
+	}
+
+	return result
+}
 
 // Unique
+func unique(arr []string) []string {
+	seen := map[string]bool{}
+	result := []string{}
+
+	for _, el := range arr {
+		_, ok := seen[el]
+		if !ok {
+			result = append(result, el)
+			seen[el] = true
+		}
+	}
+
+	return result
+}
 
 // Flatten
 
