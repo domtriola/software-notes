39b51a873697b74afff949704cd1e380e060c9cc
Dominick Triola <dominicktriola@gmail.com>
2020-07-18 12:59:37 -0700

    Add c++ notes

---
+++ languages/c++/README.md
@@ -0,0 +1,14 @@
+# C++
+
+* [Overview](#overview)
+* [Getting Started](#getting-started)
+
+## Overview
+
+C++ is a superset of C.
+
+## Getting Started
+
+* Compile a program: `g++ hello_world.cpp -o <filename (defaults to a.out)>`
+* Run the program: `./a.out`
+* [Hello World](./hello_world.cpp)
--- languages/c++/hello_world.cpp
+++ languages/c++/hello_world.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+// ^ Access standard library name iostream through header
 
+// main is the main program that is run
 int main() {
+    // Chaining commands
+    // (std::cout << "Hello World") << std::endl;
+    // (std::cout << "Hello World") returns std:cout
     std::cout << "Hello World" << std::endl;
+
+    // 0 == success, all other numbers == failure
     return 0;
 }
 
